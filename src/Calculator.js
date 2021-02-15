a1074d92a8a133ba7bc46f303597af5081ca984e
vanshika-99 <54948141+vanshika-99@users.noreply.github.com>
2021-02-15 17:08:36 -0500

    fixed minor typos

--- src/Calculator.js
+++ src/Calculator.js
@@ -11,35 +11,29 @@ class Calculator {
 
     static Sum(a,b) {
         let calculation = Calculation.Create(a,b,Sum);
-        this.addCalculation(calculation);
         return calculation;
     }
 
     static Difference(a,b) {
         let calculation = Calculation.Create(a,b,Difference);
-        this.addCalculation(calculation);
         return calculation;
     }
 
     static Product(a,b) {
         let calculation = Calculation.Create(a,b,Product);
-        this.addCalculation(calculation);
         return calculation;
     }
 
     static Quotient(a,b) {
         let calculation = Calculation.Create(a,b,Quotient);
-        this.addCalculation(calculation);
         return calculation;
     }
     static SquareRoot(a,b) {
         let calculation = Calculation.Create(a,b,SquareRoot);
-        this.addCalculation(calculation);
         return calculation;
     }
     static Square(a,b) {
         let calculation = Calculation.Create(a,b,Square);
-        this.addCalculation(calculation);
         return calculation;
     }
 
